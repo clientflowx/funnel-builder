2b8000a245ee39e0d6af60912d3990972d8f437e
namancfx <naman.shankhydhar@clientflowx.com>
2024-04-07 00:50:53 +0530

    basic project structure + google auth setup

---
+++ src/middleware.ts
@@ -0,0 +1,7 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({ publicRoutes: ["/site"] });
+
+export const config = {
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
