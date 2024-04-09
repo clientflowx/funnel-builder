c929533eb2061a04365b5ffa723a6254b383b61b
Anubhav Pal <anubhavpal15june@gmail.com>
2024-04-09 21:20:48 +0530

    feat/sidebar: Add Createbar

---
+++ src/components/sidebar/option.ts
@@ -0,0 +1,28 @@
+import { SidebarOption } from "./types";
+import { LayoutDashboard, PanelsTopLeft, Filter, Settings } from 'lucide-react';
+export const sideBarOptions: SidebarOption[] = [
+  {
+    key: "dashboard",
+    title: "Dashboard",
+    icon: LayoutDashboard,
+    titleVisible: true,
+  },
+  {
+    key: "sites",
+    title: "Sites",
+    icon: PanelsTopLeft,
+    titleVisible: true,
+  },
+  {
+    key: "funnel",
+    title: "Funnel",
+    icon: Filter,
+    titleVisible: true,
+  },
+  {
+    key: "settings",
+    title: "Settings",
+    icon: Settings,
+    titleVisible: true,
+  },
+];
