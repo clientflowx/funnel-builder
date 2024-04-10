d89ac1a01865fb09bba87c85335197d8f682a475
Anubhav Pal <anubhavpal15june@gmail.com>
2024-04-10 12:56:57 +0530

    feat/sidebar: Fix sidebar width

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -8,27 +8,23 @@ type Props = {
 };
 
 const SideBar: React.FC<Props> = ({ handleOptionChange }) => {
-    const [openSideBar, setOpenSideBar] = useState<boolean>(false);
     return (
         <div>
             <aside
                 id="logo-sidebar"
-                className={`fixed top-0 left-0 z-50 h-full transition-transform ${openSideBar ? "w-52" : "w-16"
-                    } cursor-pointer bg-black border-r border-gray-200 transition-all overflow-x-hidden`}
+                className={`fixed top-0 left-0 z-50 h-full w-52 cursor-pointer bg-black border-r border-gray-200 transition-all overflow-x-hidden`}
                 aria-label="Sidebar"
-                onMouseEnter={() => setOpenSideBar(true)}
-                onMouseLeave={() => setOpenSideBar(false)}
             >
                 <div className="h-full pb-4 overflow-y-auto bg-black">
                     <ul className="space-y-2 font-medium">
                         {sideBarOptions.map((item, index) => {
                             return (
                                 <li key={index} onClick={() => handleOptionChange(item.title)}>
-                                    <div className={`flex text-sm items-center p-2 m-2 rounded-md leading-2 text-white opacity-70 hover:opacity-100 transition-all ${openSideBar ? "" : "justify-center"} group`}>
-                                        <div className="w-5 text-white">{<item.icon />}</div>
-                                        <span className={`ms-3 h-5 ${openSideBar || !item.titleVisible ? "" : "hidden"}`}>
+                                    <div className={`flex text-sm items-center p-2 m-2 rounded-md leading-2 text-white opacity-70 hover:opacity-100 transition-all group`}>
+                                        <div className="w-5 text-white">{<item.icon size={20} />}</div>
+                                        <div className={`ms-3 h-5 ${item.titleVisible ? "" : "hidden"}`}>
                                             {item.title}
-                                        </span>
+                                        </div>
                                     </div>
                                 </li>
                             );
