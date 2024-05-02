35833c76a824e5c050f4bc2ae827d579dad67c99
namancfx <naman.shankhydhar@clientflowx.com>
2024-05-02 13:19:44 +0530

    featt((website-builder): website editor context + reducer implemented

---
+++ src/lib/constants.ts
@@ -0,0 +1,13 @@
+export type EditorBtns =
+  | "text"
+  | "container"
+  | "section"
+  | "contactForm"
+  | "paymentForm"
+  | "link"
+  | "2Col"
+  | "video"
+  | "__body"
+  | "image"
+  | "3Col"
+  | null;
